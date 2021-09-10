1fb43350875449055cb76616bf1efe70cd4034b3
Iran Braga <iran.brg@gmail.com>
2021-09-10 09:52:42 -0300

    chore: setup typeorm

---
+++ src/database/index.ts
@@ -0,0 +1,6 @@
+import { createConnection } from "typeorm";
+
+// TODO: only start the server after the database is successfully connected
+createConnection()
+    .then(() => console.log("Succesfully connected to the database"))
+    .catch(err => console.log(err));
