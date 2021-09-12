fcf493e22ab7ce6913cb697f87d296e046bc7941
Iran Braga <iran.brg@gmail.com>
2021-09-11 23:35:14 -0300

    refactor: remove query for adding uuid extention on CreateUsers migration

--- src/database/migrations/1631233933405-CreateUsers.ts
+++ src/database/migrations/1631233933405-CreateUsers.ts
@@ -3,8 +3,6 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 export class CreateUsers1631233933405 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
-
         await queryRunner.createTable(new Table({
             name: "users",
             columns: [
@@ -84,8 +82,5 @@ export class CreateUsers1631233933405 implements MigrationInterface {
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.dropTable("users", true);
-
-        await queryRunner.query('DROP EXTENSION IF EXISTS "uuid-ossp";');
     }
-
 }
