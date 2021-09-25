d19a4554d7596ed6433c965e3fff113983622ce4
Iran Braga <iran.brg@gmail.com>
2021-09-25 19:35:17 -0300

    ci: fix execution of migrations for staging
    Set SSL connection to the database to false for enviroments other than prod

--- ormconfig.js
+++ ormconfig.js
@@ -8,6 +8,10 @@ module.exports = {
     logging: process.env.NODE_ENV !== "test",
     entities: [process.env.TYPEORM_ENTITIES],
     migrations: [process.env.TYPEORM_MIGRATIONS],
+    ssl:
+        process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
     cli: {
         entitiesDir: "src/entities",
         migrationsDir: "src/database/migrations"
