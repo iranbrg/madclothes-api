268de1c3d33d29254664e83119a481dc95d128a5
Iran Braga <iran.brg@gmail.com>
2021-09-25 17:17:44 -0300

    feat: create new 404 (resource not found) route

---
+++ src/routes/v1/notFoundRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { HTTPException } from "../../utils/errors";
+import { HTTP } from "../../utils/constants";
+
+const router = Router();
+
+router.use(function(req, res) {
+    throw new HTTPException("Not found", HTTP.NotFound);
+});
+
+export default router;
