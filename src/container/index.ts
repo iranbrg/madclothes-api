6f359592a9c5af2f279720173c080106d5b05aeb
Iran Braga <iran.brg@gmail.com>
2021-10-07 21:01:14 -0300

    feat: add list of customers on cache

--- src/container/index.ts
+++ src/container/index.ts
@@ -1,5 +1,11 @@
 import { container } from "tsyringe";
+
 import UserRepository from "../repositories/implementations/UserRepository";
 import IUserRepository from "../repositories/IUserRepository";
 
+import ICacheProvider from "../providers/ICacheProvider";
+import RedisCacheProvider from "../providers/implementations/RedisCacheProvider";
+
 container.registerSingleton<IUserRepository>("UserRepository", UserRepository);
+
+container.registerSingleton<ICacheProvider>("CacheProvider", RedisCacheProvider);
--- src/services/ListCustomersService.ts
+++ src/services/ListCustomersService.ts
@@ -1,16 +1,26 @@
 import { inject, injectable } from "tsyringe";
 import User from "../entities/User";
+import ICacheProvider from "../providers/ICacheProvider";
 import IUserRepository from "../repositories/IUserRepository";
 
 @injectable()
 export default class ListCustomersService {
     constructor(
         @inject("UserRepository")
-        private userRepository: IUserRepository
-    ) {}
+        private userRepository: IUserRepository,
+
+        @inject("CacheProvider")
+        private cacheProvider: ICacheProvider
+    ) { }
 
     public async execute(): Promise<Omit<User, "password">[]> {
-        const customers = await this.userRepository.findAllCustomers();
+        let customers = await this.cacheProvider.get<User[]>("users:customers");
+
+        if (!customers) {
+            customers = await this.userRepository.findAllCustomers();
+
+            await this.cacheProvider.set("users:customers", customers);
+        }
 
         const customersWithoutPassword = customers.map(customer => {
             const { password, ...customerWithoutPassword } = customer;
